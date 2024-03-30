0d47ea0f1c7fb041ed69aac75c102142ef4d8fd7
goslingscripts <150567568+goslingscripts@users.noreply.github.com>
2024-03-30 12:15:58 +0800

    Update jjc_playerstats.lua

--- jjc_playerstats.lua
+++ jjc_playerstats.lua
@@ -1,37 +1,37 @@
 local plr = game.Players.LocalPlayer
 
-local function plrName()
+ function plrName()
     return tostring(plr.Name)
 end
 
-local function enduranceLevel()
+ function enduranceLevel()
     if plr.PlayerGui:FindFirstChild("jujuMain") then
         local lvl = plr.PlayerGui.jujuMain.hud.stat.stats.primary.Endurance.Level.Text
         return tonumber(lvl)
     end
 end
 
-local function ceLevel()
+ function ceLevel()
     if plr.PlayerGui:FindFirstChild("jujuMain") then
         local lvl = plr.PlayerGui.jujuMain.hud.stat.stats.primary.CursedEnergy.Level.Text
         return tonumber(lvl)
     end
 end
 
-local function strengthLevel()
+ function strengthLevel()
     if plr.PlayerGui:FindFirstChild("jujuMain") then
         local lvl = plr.PlayerGui.jujuMain.hud.stat.stats.primary.Fist.Level.Text
         return tonumber(lvl)
     end
 end
 
-local function staminaLevel()
+ function staminaLevel()
     if plr.PlayerGui:FindFirstChild("jujuMain") then
         local lvl = plr.PlayerGui.jujuMain.hud.stat.stats.primary.Stamina.Level.Text
         return tonumber(lvl)
     end
 end
 
-local function currentGrade()
+ function currentGrade()
     return tostring(plr.leaderstats.Grade.Value)
 end
